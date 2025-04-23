2920ccea135e6559feac7b64c86d9e5708104f18
Anish Poudel <anishpoudel007@gmail.com>
2025-04-23 17:39:39 +0545

    updated

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,4 +4,4 @@ echo "Running migrations..."
 python manage.py migrate
 
 echo "Starting Gunicorn..."
-exec gunicorn project.wsgi:application --bind 0.0.0.0:8000
+exec gunicorn --workers $(nproc) project.wsgi:application --bind 0.0.0.0:8000
